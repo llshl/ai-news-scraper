702f17a13ae696e35625ca8250c9febbb6cd55cd
llshl <lsh970708@naver.com>
2024-11-05 02:58:05 +0900

    init

---
+++ src/main/java/com/imsh/ainewsscraper/AiNewsScraperApplication.java
@@ -0,0 +1,13 @@
+package com.imsh.ainewsscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNewsScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNewsScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imsh/ainewsscraper/AiNewsScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imsh.ainewsscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNewsScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
